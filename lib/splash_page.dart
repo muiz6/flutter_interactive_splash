39c3818d9df242d8e0ffe78292ff1adae0c21701
muiz6 <muizhassan83@gmail.com>
2021-05-26 13:05:21 +0500

    fixed style

--- lib/splash_page.dart
+++ lib/splash_page.dart
@@ -16,28 +16,40 @@ class _SplashPageState extends State<SplashPage> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Image.asset('assets/images/flutter_logo.png'),
+      child: Center(
+        child: Image.asset(
+          'assets/images/logo_flutter.png',
+          height: 168.94,
+        ),
+      ),
     );
   }
 
   void _afterBuild(BuildContext context) {
-    showDialog(
+    showModalBottomSheet(
       context: context,
-      barrierDismissible: false,
+      isDismissible: false,
+      enableDrag: false,
       barrierColor: Colors.transparent,
+      backgroundColor: Colors.blue,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+        topLeft: const Radius.circular(25),
+        topRight: const Radius.circular(25),
+      )),
       builder: (builderContext) {
-        return Padding(
-          padding: EdgeInsets.all(16),
-          child: Center(
-            child: Card(
-              child: Column(
-                children: [
-                  Text('Press OK to dismiss'),
-                  ElevatedButton(
-                    onPressed: () => Navigator.pop(builderContext),
-                    child: Text('OK'),
-                  ),
-                ],
+        return SizedBox(
+          height: 100,
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  onPrimary: Colors.blue,
+                ),
+                onPressed: () => Navigator.pop(context),
+                child: Text('Dismiss'),
               ),
             ),
           ),
